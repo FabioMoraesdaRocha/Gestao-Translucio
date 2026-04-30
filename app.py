dd80330c503be115436089c90dc694104576a85f
FabioMoraesdaRocha <controladoria@translucio.com.br>
2026-04-30 19:52:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Teste App 🚀")
+
+st.write("Se você está vendo isso, o app está funcionando")
+
+url = "https://docs.google.com/spreadsheets/d/1eX28N9DyCgR8MTD6Ol8MtHOJQ1e3GPamw5KMGRxJnmU/gviz/tq?tqx=out:csv&sheet=Dados"
+
+try:
+    df = pd.read_csv(url)
+    st.write("Dados carregados com sucesso ✅")
+    st.dataframe(df)
+
+except Exception as e:
+    st.error("Erro ao carregar dados ❌")
+    st.write(e)
